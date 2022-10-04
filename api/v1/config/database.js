2c5298a756b641b0f9ab80ecbb1b3fcf9ce09bd7
vandeco <hathaoeagle@gmail.com>
2022-10-04 13:41:50 +0800

    update DataBase

--- api/v1/config/database.js
+++ api/v1/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB= async()=>{
   try {
-    await mongoose.connect("mongodb://localhost:27017/demo4")
+    await mongoose.connect("mongodb+srv://itvinatt:1007mccl@cluster0.yyn9vwv.mongodb.net/?retryWrites=true&w=majority")
     console.log("Connect DB successful")
   } catch (error) {
     console.log("Lỗi kết nối db: "+ error.message)
