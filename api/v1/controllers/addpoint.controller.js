a9c746437fcceafbb8b422839dcbf8f249b405e0
vandeco <hathaoeagle@gmail.com>
2022-10-06 01:23:38 +0800

    a

--- api/v1/controllers/addpoint.controller.js
+++ api/v1/controllers/addpoint.controller.js
@@ -3,29 +3,32 @@ const axios = require('axios')
 
 module.exports = {
     addpoint: async(req,res,next)=>{
-        var data = '{"manualAdjustments":[{"playerid": "ceshi01","adjustamt": "1","turnovervalue": "1","removegwc": false,"servicefee": "1","adminfeeratio": "1","turnovertype": "0","ecremarks": "Tes2","remarks": "Tes1","reasontype": "2","manualtype": "1", "walletid": "MAIN"}],"sendmessage": true,messages": {"msgtype": "2","subject": "Test3","content": "<p>Test4</p>","players": "ceshi01"}}';
-        var config = {
-        method: 'post',
-        url: 'https://boapi.jun88.bet/jun88-ims/api/v1/manualadjusts/batch',
-        headers: { 
-            'accept': ' */*', 
-            'accept-encoding': ' gzip, deflate, br', 
-            'accept-language': ' vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5', 
-            'authorization': 'eyJhbGciOiJIUzI1NiJ9.ewogICJpYXQiIDogMTY2NDk3MDUzNiwKICAiZXhwIiA6IDE2NjQ5ODEzMzYsCiAgInVzZXJuYW1lIiA6ICJqdW44ODk5NiIsCiAgImVtYWlsIiA6IG51bGwsCiAgInJvbGUiIDogIiIsCiAgImV4dHJhIiA6ICJ7fSIsCiAgImlzb3AiIDogdHJ1ZQp9.PM9cw-2qjsJoQ3n1E77iCwm2TsE_fdSFjcu3LEud2rI', 
-            'content-type': ' application/json;charset=UTF-8', 
-            'origin': ' https://bo.jun88.bet', 
-            'referer': ' https://bo.jun88.bet/'
-        },
-        data : data
-        };
-
-        axios(config)
-        .then(function (response) {
-            res.json(response.data);
-        })
-        .catch(function (error) {
-        console.log(error);
-        });
+    let {...body} = req.body
+    // var data = '{\r\n  "manualAdjustments": [\r\n    {\r\n      "playerid": "ceshi01",\r\n      "adjustamt": "1",\r\n      "turnovervalue": "1",\r\n      "removegwc": false,\r\n      "servicefee": "1",\r\n      "adminfeeratio": "1",\r\n      "turnovertype": "0",\r\n      "ecremarks": "Tes2",\r\n      "remarks": "Tes1",\r\n      "reasontype": "2",\r\n      "manualtype": "1",\r\n      "walletid": "MAIN"\r\n    }\r\n  ],\r\n  "sendmessage": true,\r\n  "messages": {\r\n    "msgtype": "2",\r\n    "subject": "Test3",\r\n    "content": "<p>Test4</p>",\r\n    "players": "ceshi01"\r\n  }\r\n}';
+    
+    var config = {
+      method: 'post',
+      url: 'https://boapi.jun88.bet/jun88-ims/api/v1/manualadjusts/batch',
+      headers: { 
+        'accept': ' */*', 
+        'accept-encoding': ' gzip, deflate, br', 
+        'accept-language': ' vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5', 
+        'authorization': 'eyJhbGciOiJIUzI1NiJ9.ewogICJpYXQiIDogMTY2NDk3MDUzNiwKICAiZXhwIiA6IDE2NjQ5ODEzMzYsCiAgInVzZXJuYW1lIiA6ICJqdW44ODk5NiIsCiAgImVtYWlsIiA6IG51bGwsCiAgInJvbGUiIDogIiIsCiAgImV4dHJhIiA6ICJ7fSIsCiAgImlzb3AiIDogdHJ1ZQp9.PM9cw-2qjsJoQ3n1E77iCwm2TsE_fdSFjcu3LEud2rI', 
+        'content-type': ' application/json;charset=UTF-8', 
+        'origin': ' https://bo.jun88.bet', 
+        'referer': ' https://bo.jun88.bet/'
+      },
+      data : body
+    };
+    
+    axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
     }
 }
 
